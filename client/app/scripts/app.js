55bca5d1005121259973c164db7854f5e3e359ff
root <aito0077@gmail.com>
2016-02-11 08:59:16 -0300

    deploy

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -39,6 +39,6 @@ angular
   })
 .filter('moment', function() {
     return function(dateString, format) {
-        return moment(dateString).format(format);
+        return moment(dateString).locale('es').format(format);
     };
 });
